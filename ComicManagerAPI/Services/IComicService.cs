b61582e9db14731868314334f7d7b81e41f48df3
ceemeese <cmsalvador92@gmail.com>
2025-05-12 20:29:27 +0200

    Add service interfaces for entities

---
+++ ComicManagerAPI/Services/IComicService.cs
@@ -0,0 +1,13 @@
+using Models;
+
+namespace ComicManagerAPI.Data
+{
+    public interface IComicService 
+    {
+        Task<List<Comic>> GetAllAsync();
+        Task<Comic> GetByIdAsync(int id);
+        Task AddAsync(Comic comic);
+        Task UpdateAsync(Comic comic);
+        Task DeleteAsync(int id);
+    }
+}
