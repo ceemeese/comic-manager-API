aa8356b0579d3c61669a29b396c5260badff4f10
ceemeese <cmsalvador92@gmail.com>
2025-05-14 12:43:50 +0200

    Add interfaces services for relationship tables

---
+++ ComicManagerAPI/Services/IComicGenreService.cs
@@ -0,0 +1,13 @@
+using Models;
+
+namespace ComicManagerAPI.Data
+{
+    public interface IComicGenreService
+    {
+        Task AddAsync(ComicGenre comicGenre);
+        Task DeleteAsync(int comicId, int genreId);
+        Task ExistsAsync(int comicId, int genreId);
+        Task<List<Genre>> GetGenresByComicIdAsync(int comicId);
+        Task<List<Comic>> GetComicsByGenreIdAsync(int genreId);
+    }
+}
